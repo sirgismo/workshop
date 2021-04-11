68f6d992f71472b69130af6941d25f651da31ee9
DESKTOP-2GC8JRG\pooya <pooyasabeti97@yahoo.com>
2021-04-11 18:04:56 +0430

    Add new scene and button to load another scene

---
+++ Assets/Scripts/SelectScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SelectScene : MonoBehaviour
+{
+    public Texture btnTexture;
+    // Start is called before the first frame update
+
+
+    void OnGUI()
+    {
+        if (!btnTexture)
+        {
+            Debug.LogError("Please assign a texture on the inspector");
+            return;
+        }
+
+        if (GUI.Button(new Rect(300, 100, 500, 200), "GO TO SCENE "))
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
