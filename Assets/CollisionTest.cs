9446de419130fb6422f5db6a9a51c92699b72433
Pouya Sabeti <pooyasabeti97@yahoo.com>
2021-04-15 15:31:09 +0430

    Working on collision trigger and wall jump ( not completed )

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    
+    Material material;
+    Color dangerColor;
+    Color safeColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = gameObject.GetComponent<MeshRenderer>().material;
+        dangerColor = new Color(50,50,50,1);
+        safeColor = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DangerColor()
+    {
+        material.color = dangerColor;
+    }
+    public void SafeColor()
+    {
+        material.color = safeColor;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,33 +4,86 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    enum State { onWall ,onGround, onAir }
     [SerializeField] int power;
     Rigidbody _rigidbody;
     [SerializeField] int speed;
-     
-    
-    
+    int jumpCount;
+    State playerState;
+    RaycastHit rayhit;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        power = 200;
-        speed = 200;
+        // float MaxHeight;
+        jumpCount = 2;
+        
         _rigidbody =  GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Physics.Raycast(transform.position, transform.forward, out rayhit))
+        {
+            rayhit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            Debug.Log(rayhit.collider.name);
+            Debug.Log("HIIIIIIIT!!!!");
+        }
         PlayerMovement();
+       //MaxHeight =  _rigidbody.transform.position.
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        Debug.Log("enter collision");
+        if (collision.collider.tag == "ground")
+        {
+            playerState = State.onGround;
+            jumpCount = 2;
+        }
+        else if (collision.collider.tag == "wall")
+        {
+            
+            playerState = State.onWall;
+            jumpCount = 1;
+        }
+       
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("exit collision");
+        
+    }
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter");
+        Debug.Log(_rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x,-_rigidbody.velocity.y,_rigidbody.velocity.z);
+        
+    }
+ 
+   
     private void PlayerMovement()
     {
+        
         Vector3 force = new Vector3(0,0,0);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount>0)
         {
+            jumpCount--;
             force += Vector3.up * power;
+            if (playerState == State.onWall && _rigidbody.velocity.x > 0)
+            {
+                force += Vector3.left * power;
+            }
+            if (playerState == State.onWall && _rigidbody.velocity.x < 0)
+            {
+                force += Vector3.right * power;
+            }
+
+            
             //_rigidbody.AddForce(Vector3.up * power);
         }
         if (Input.GetKey(KeyCode.UpArrow))
