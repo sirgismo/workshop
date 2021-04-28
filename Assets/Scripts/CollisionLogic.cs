f5d0082a964e8b077de0372fea9a22a485327524
DESKTOP-2GC8JRG\pooya <pooyasabeti97@yahoo.com>
2021-04-29 00:17:16 +0430

    add strategic_movement with coroutine

---
+++ Assets/Scripts/CollisionLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionLogic : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GameObject.FindWithTag("player").GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StrategicMove.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrategicMove : MonoBehaviour
+{
+    [SerializeField] private int MovementSpeed;
+    public new Camera  camera;
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject selectedPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MovementSpeed = 1000;
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ray = camera.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+            Debug.Log(hit.collider.name);
+            // SelectPlayer();
+            if (selectedPlayer)
+            {
+                Debug.Log(selectedPlayer.name);
+                if (hit.collider.gameObject == selectedPlayer)
+                {
+                    Debug.Log(selectedPlayer.name);
+                    selectedPlayer = null;
+                }
+                else
+                {
+                    Vector3 toPos = new Vector3(hit.point.x,0,hit.point.z);
+                    Debug.Log(selectedPlayer.name);
+                    Debug.Log(toPos);
+                    StopAllCoroutines();
+                    StartCoroutine(MoveTo(toPos));
+                }
+                
+            }
+            else if (hit.collider.gameObject.tag == "Player")
+            {
+                selectedPlayer = hit.collider.gameObject;
+            }
+        }
+
+    }
+
+   private IEnumerator MoveTo(Vector3 dest)
+    {
+        Debug.Log("enter Moveto");
+        Vector3 playerPos = selectedPlayer.transform.position;
+
+        //Vector3 distance;
+        //MoveDir.Normalize();
+
+        while ((dest-playerPos).magnitude>1)
+        {
+            playerPos = selectedPlayer.transform.position;
+            Vector3 MoveDir = dest - playerPos;
+            MoveDir = new Vector3(MoveDir.x, 0, MoveDir.z);
+            Debug.Log(MovementSpeed * Time.deltaTime * MoveDir);
+            Debug.Log("enterWhile");
+            selectedPlayer.GetComponent<Rigidbody>().AddForce(MovementSpeed * Time.deltaTime * MoveDir);
+            yield return null;
+
+        }
+    }
+}
