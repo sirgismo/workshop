18fcc1df2406912f446dd25470f68182f789cd13
Pouya Sabeti <pooyasabeti97@yahoo.com>
2021-04-29 20:19:40 +0430

    session 8 of workshop including Vuforia SDK

--- Assets/Scripts/StrategicMove.cs
+++ Assets/Scripts/StrategicMove.cs
@@ -9,9 +9,11 @@ public class StrategicMove : MonoBehaviour
     private Ray ray;
     private RaycastHit hit;
     private GameObject selectedPlayer;
+    [SerializeField]  private float scrollSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        scrollSpeed = 2;
         MovementSpeed = 1000;
         camera = Camera.main;
     }
@@ -19,7 +21,17 @@ public class StrategicMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            float zoom = Input.GetAxis("Mouse ScrollWheel");
+            camera.transform.localPosition += new Vector3(0,0,zoom*scrollSpeed);
+        }
+        SelectAndMove();
 
+    }
+
+    private void SelectAndMove()
+    {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             ray = camera.ScreenPointToRay(Input.mousePosition);
@@ -36,37 +48,37 @@ public class StrategicMove : MonoBehaviour
                 }
                 else
                 {
-                    Vector3 toPos = new Vector3(hit.point.x,0,hit.point.z);
+                    Vector3 toPos = new Vector3(hit.point.x, 0, hit.point.z);
                     Debug.Log(selectedPlayer.name);
                     Debug.Log(toPos);
                     StopAllCoroutines();
                     StartCoroutine(MoveTo(toPos));
                 }
-                
+
             }
             else if (hit.collider.gameObject.tag == "Player")
             {
                 selectedPlayer = hit.collider.gameObject;
             }
         }
-
     }
 
-   private IEnumerator MoveTo(Vector3 dest)
+    private IEnumerator MoveTo(Vector3 dest)
     {
-        Debug.Log("enter Moveto");
+       // Debug.Log("enter Moveto");
         Vector3 playerPos = selectedPlayer.transform.position;
-
+        Vector3 distance = dest - playerPos;
+        
+        
         //Vector3 distance;
         //MoveDir.Normalize();
-
+        
         while ((dest-playerPos).magnitude>1)
         {
             playerPos = selectedPlayer.transform.position;
             Vector3 MoveDir = dest - playerPos;
             MoveDir = new Vector3(MoveDir.x, 0, MoveDir.z);
-            Debug.Log(MovementSpeed * Time.deltaTime * MoveDir);
-            Debug.Log("enterWhile");
+            
             selectedPlayer.GetComponent<Rigidbody>().AddForce(MovementSpeed * Time.deltaTime * MoveDir);
             yield return null;
 
